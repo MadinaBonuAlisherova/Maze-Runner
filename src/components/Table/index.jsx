0c56a3ad3fb9959a557747588cda86d5f74c28b1
u1810291 <o.rizakulov@student.inha.uz>
2020-12-04 18:43:04 +0500

    Addede components

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,12 @@
+import { Container, Body, Header, Footer } from "./style";
+
+// eslint-disable-next-line
+export default () => {
+  return (
+    <Container>
+      <Header>Maze runner</Header>
+      <Body>Body</Body>
+      <Footer>Footer</Footer>
+    </Container>
+  );
+};
---
+++ src/components/Table/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const Header = styled.span``;
+export const Body = styled.div``;
+export const Footer = styled.div``;
