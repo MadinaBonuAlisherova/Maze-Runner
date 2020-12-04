3545312da03f4b97904b7f528feace0b064fe4cc
u1810291 <o.rizakulov@student.inha.uz>
2020-12-04 21:11:27 +0500

    some changes

--- src/views/Maze/index.jsx
+++ src/views/Maze/index.jsx
@@ -1,11 +1,21 @@
-import { Container, Body, Header, Footer } from "./style";
+import { Container, Body, Header, Footer, Title } from "./style";
+import Box from "../../components/Box";
+import Button from "../../components/Button";
+import Table from "../../components/Table";
 
 // eslint-disable-next-line
 export default () => {
   return (
     <Container>
-      <Header>Maze runner</Header>
-      <Body>Body</Body>
+      <Header>
+        <Title>Maze runner</Title>
+      </Header>
+      <Body>
+        <Title>Body</Title>
+        <Box children="Box" />
+        <Button children="Button" />
+        <Table children="Table" />
+      </Body>
       <Footer>Footer</Footer>
     </Container>
   );
